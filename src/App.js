d898da7cadbc01b711c76897028e13d5f7f4b108
amanRecreates <amanofficial950@gmail.com>
2021-07-19 14:34:27 +0530

    Creating and saving notes locally on frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import style from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [notes, setNotes] = useState([])
+    const [body, setBody] = useState('')
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+        if (body) {                // to ignore empty field save
+            console.log(body);
+            let note = {
+                "body": body,
+                "id": notes.length,
+            }
+            let tempNotes = notes
+            tempNotes.push(note)
+            setNotes(tempNotes)
+            setBody('')
+        }
+    }
+
+    return (
+        <div className={style.app}>
+            <form onSubmit={handleSubmitForm}>
+                <label>Note</label>
+                <input type="text" value={body} onChange={(e) => setBody(e.target.value)}></input>
+                <button>Save</button>
+            </form>
+            <div className={style.notes}>
+                <h2>Notes</h2>
+                {notes.map(note => <p key={note.id}>{note.body}</p>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
