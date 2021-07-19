0eeb5434c7432c9b3abebb20cafbcc47703f4c4e
amanRecreates <amanofficial950@gmail.com>
2021-07-19 20:33:06 +0530

    added auto-focus & Escape Keydown evenlistner

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
 
     const [notes, setNotes] = useState(null)
     const [body, setBody] = useState('')
+    const [title, setTitle] = useState('')
 
+    const autofocus = () => {
+        document.getElementById('focus-req').focus()
+    }
+
+    document.onkeydown = (e) => {
+        if (e.key === 'Escape') {
+            autofocus()
+        }
+    }
 
     useEffect(() => {
         fetch('http://localhost:8000/notes')
@@ -15,31 +25,33 @@ function App() {
             .then(data => {
                 setNotes(data)
             })
+        document.getElementById('focus-req').focus()
     }, [])
 
     const handleSubmitForm = (e) => {
         e.preventDefault()
-        if (body) {                // to ignore empty field save
-            const note = { body }
+        const note = { title, body }
 
-            // POST request, to update the displayed list
-            fetch('http://localhost:8000/notes', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(note)
-            }).then(() => {
-                    // Get request, to update the displayed list
-                    fetch('http://localhost:8000/notes')
-                        .then(res => {
-                            return res.json()
-                        })
-                        .then(data => {
-                            setNotes(data)
-                        })
+        // POST request, to update the displayed list
+        fetch('http://localhost:8000/notes', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(note)
+        }).then(() => {
+            // Get request, to update the displayed list
+            fetch('http://localhost:8000/notes')
+                .then(res => {
+                    return res.json()
                 })
-        }
+                .then(data => {
+                    setNotes(data)
+                })
+        })
+        setBody('')
+        setTitle('')
+        autofocus() 
     }
 
     const handleDelete = (id) => {
@@ -60,19 +72,21 @@ function App() {
     return (
         <div className={style.app}>
             <form onSubmit={handleSubmitForm}>
-                <label>Note</label>
-                <input type="text" value={body} onChange={(e) => setBody(e.target.value)}></input>
-                <p>{body}</p>
+                <label>Description</label>
+                <input id="focus-req" required type="text" value={body} onChange={(e) => setBody(e.target.value)}></input>
+                <label>Title</label>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
                 <button>Save</button>
             </form>
             <div className={style.notes}>
                 <h2>Notes</h2>
                 {notes && notes.map(note => {
                     return (
-                        <p key={note.id}>
-                            {note.body}
+                        <div key={note.id}>
+                            <h2>{note.title}</h2>
+                            <p>{note.body}</p>
                             <button onClick={() => handleDelete(note.id)}>Delete</button>
-                        </p>
+                        </div>
                     )
                 }
                 )}
